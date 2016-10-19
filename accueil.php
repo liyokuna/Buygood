1c63ce9ede6c717df5b739b0bbf99e4d183ae583
Lionel MUKUNA CIOWELA <liyokuna@yahoo.fr>
2016-10-19 13:49:33 +0200

    Initial commit with contributors

---
+++ accueil.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Best or Bad Trip - Welcome </title>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link  rel="icon" href="www/pictures/icon1.png" type="image/png" />
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<!--Latest compiled and minified CSS from PureCSS-->
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		<span class ="icon-bar"></span>
+		<span class ="icon-bar"></span>
+		<span class ="icon-bar"></span>	
+	</button>
+	
+    <a class="navbar-brand" href="#">Best Trip</a>
+    </div>
+	<div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav">
+		<li>
+		<a href="#">Home</a>
+		<a href="#">Contact</a>
+		</li> 
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Users</a></li>
+    </ul>
+	</div>
+  </div>
+</nav>
+
+<div class="container-fluid">
+blabla
+
+</div>
+
+
+
+<footer class="container-fuild text-center bg-grey" style="position: fixed;">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <div class="footer-above">
+	<div class="container">
+		<div class="row">
+                <div class="pure-menu pure-menu-horizontal text-right">
+                    <ul>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Facebook</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Twitter</a></li>
+						<li class="pure-menu-item"><a href="#" class="pure-menu-link">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+		</div>
+	</div>
+	<div class="footer-below">	
+		<div class="container text-left">
+			<div class="row">
+		<p>Copyright 2016 - Best Trip</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Best or Bad Trip - Welcome </title>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link  rel="icon" href="www/pictures/icon1.png" type="image/png" />
+<link rel="stylesheet" type="text/css" href="style.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<!--Latest compiled and minified CSS from PureCSS-->
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="container-fluid">
+	<div class="centre">
+		<div class="panelBack">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+				<p>
+					<a href="#" hreflang="en" lang="en">
+						<button type="button" class="btn btn-default btn-lg btn-block" >Log In</button>
+					</a>
+				</p>
+				</div>
+	
+			</div>
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+				<p>
+				<a href="#" hreflang="fr" lang="fr">
+					<button type="button" class="btn btn-default btn-lg btn-block" >Sign Up</button>
+				</a>
+				</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
